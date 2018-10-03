85bed2836d553b32b5c63a0b99bfcf7493bf158c
Konstantin <konstantin@localhost.systems>
2018-10-03 19:05:40 +0200

    add paralell sieve of eras.

--- ConcurrentPrimeCalculator/main.cpp
+++ ConcurrentPrimeCalculator/main.cpp
@@ -2,24 +2,36 @@
 #include <math.h>
 #include <vector>
 #include <thread>
+#include <mutex>
 
 using namespace std; 
 
-void ParallellWorker(bool (*validPrims), int toCheck, int maxToCheck) {
+std::mutex mylock;
+vector<bool> validPrims;
+
+void ParallellWorker(vector<bool> &validPrims, int toCheck, int maxToCheck) {
     if (toCheck == 2) {
         for (int i = 2; toCheck * i <= maxToCheck; i++) {
-            (*validPrims)[toCheck * i] = true;
+            mylock.lock();
+            validPrims[toCheck * i] = true;
+            mylock.unlock();
         }
     } else {
         for (int i = 3; toCheck * i <= maxToCheck; i += 2) {
-            (*validPrims)[toCheck * i] = true;
+            mylock.lock();
+            validPrims[toCheck * i] = true;
+            mylock.unlock();
         }
     }
+
+
 }
 
 int Check(int maxToCheck) {
-    bool p[maxToCheck + 1] = {false};
-    bool (*validPrims)[maxToCheck + 1] = &p;
+    static const int size = maxToCheck + 1;
+
+    validPrims.assign(size, false);
+    // bool validPrims[maxToCheck + 1] = &p;
 
     int lastToRemove = sqrt(maxToCheck);
     cout << "LastToRemove: " << lastToRemove << endl;
@@ -27,36 +39,42 @@ int Check(int maxToCheck) {
     // Remove everything before lastToRemove Single-Threaded
     int currentPrim = 2;
     while (currentPrim <= lastToRemove) {
-        for (int i = 2; currentPrim * i <= lastToRemove; i++) {
-            (*validPrims)[currentPrim * i] = true;
-        }
+        for (int i = 2; currentPrim * i <= lastToRemove; i++)
+            validPrims[currentPrim * i] = true;
 
         // find next prim to remove
         currentPrim += 1;
-        while ((*validPrims)[currentPrim] == true) {
-            currentPrim += 1;
-        }            
+        while (validPrims[currentPrim] == true)
+            currentPrim += 1;      
     }
     
     vector<int> primsBeforeLastToCheck;
-    for (int i = 2; i < lastToRemove; i++) {
-        if ((*validPrims)[i] == false) {
+    for (int i = 2; i < lastToRemove; i++)
+        if (validPrims[i] == false)
             primsBeforeLastToCheck.push_back(i);
-        }
-    }
 
     vector<thread> threads;
     // Now remove mutiples of 0 - lastToRemove
-    for(auto const& toCheck: primsBeforeLastToCheck) {
-        thread calcThread(&ParallellWorker, validPrims, toCheck, maxToCheck);
-        calcThread.detach();
-        threads.push_back(calcThread);
-    }           
+    for (auto &toCheck: primsBeforeLastToCheck) {
+        std::thread t(ParallellWorker, std::ref(validPrims), toCheck, maxToCheck);
+        threads.push_back(move(t));
+    }
 
+    for (auto &t: threads)
+        t.join();
+
+    int amount = 0;
+    for (int i = 2; i < maxToCheck; i++) {
+        if (validPrims[i] == false) {
+            cout << i << endl;
+            amount++;
+        }
+    }
+    cout << "Amount: " << amount << endl;  
 }
 
 int main() {
-    Check(1000);
+    Check(100);
 
     return 0;
 }
