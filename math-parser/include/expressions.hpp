fe7740017ce0e6afd5eb022fa8fd6d1eac422b57
Koka <konstantin@localhost.systems>
2019-04-17 11:32:19 +0200

    Update expressions.hpp

--- math-parser/include/expressions.hpp
+++ math-parser/include/expressions.hpp
@@ -1,82 +1,133 @@
-#ifndef EXPRESSIONS_HPP
-#define EXPRESSIONS_HPP
+#pragma once
 
 #include <string>
 
 class Expression
 {
-  public:
-    virtual double evaluate() = 0;
-    virtual ~Expression() = 0; // Virtual deconstructor definieren, cauz: ‘class Expression’ has virtual functions and accessible non-virtual destructor
-};
-
-class Addition : public Expression
-{
-  public:
-    Addition(Expression &_left, Expression &_right) : left{_left}, right{_right} {};
-    double evaluate() override
-    {
-        return left.evaluate() + right.evaluate();
-    };
-
-  private:
-    Expression &left;
-    Expression &right;
-};
-
-class Substraction : public Expression
-{
-  public:
-    Substraction(Expression &_left, Expression &_right) : left{_left}, right{_right} {};
-    double evaluate() override
-    {
-        return left.evaluate() - right.evaluate();
-    };
-
-  private:
-    Expression &left;
-    Expression &right;
-};
-
-class Multiplication : public Expression
-{
-  public:
-    Multiplication(Expression &_left, Expression &_right) : left{_left}, right{_right} {};
-    double evaluate() override
-    {
-        return left.evaluate() * right.evaluate();
-    };
-
-  private:
-    Expression &left;
-    Expression &right;
-};
-
-class Division : public Expression
-{
-  public:
-    Division(Expression &_left, Expression &_right) : left{_left}, right{_right} {};
-    double evaluate() override
-    {
-        return left.evaluate() / right.evaluate();
-    };
-
-  private:
-    Expression &left;
-    Expression &right;
+public:
+  virtual double evaluate() = 0;
+  virtual ~Expression(){}; // Virtual deconstructor definieren, cauz: ‘class Expression’ has virtual functions and accessible non-virtual destructor
+  virtual void setLeft(Expression *left) = 0;
+  virtual void setRight(Expression *right) = 0;
 };
 
 class Terminal : public Expression
 {
-  public:
-    Terminal(double _num) : num{_num} {}
-    double evaluate() override
-    {
-        return num;
-    };
+public:
+  Terminal(double _num) : num{_num} {}
+  Terminal() = default;
+  virtual double evaluate()
+  {
+    return num;
+  };
 
-  private:
-    double num;
+  void setNum(double _num)
+  {
+    num = _num;
+  }
+
+  virtual void setLeft(Expression *) {}
+  virtual void setRight(Expression *) {}
+
+private:
+  double num;
 };
 
-#endif
+Terminal *EMPTY_TERMINAL = new Terminal{0.0};
+
+class Addition : public Expression
+{
+public:
+  Addition(Expression *_left, Expression *_right) : left{_left}, right{_right} {};
+  virtual double evaluate()
+  {
+    return left->evaluate() + right->evaluate();
+  };
+
+  virtual void setLeft(Expression *_left)
+  {
+    left = _left;
+  }
+  virtual void setRight(Expression *_right)
+  {
+    right = _right;
+  }
+
+private:
+  Expression *left;
+  Expression *right;
+};
+
+class Substraction : public Expression
+{
+public:
+  Substraction(Expression *_left, Expression *_right) : left{_left}, right{_right} {};
+  virtual double evaluate()
+  {
+    return left->evaluate() - right->evaluate();
+  };
+
+  virtual void setLeft(Expression *_left)
+  {
+    left = _left;
+  }
+  virtual void setRight(Expression *_right)
+  {
+    right = _right;
+  }
+
+private:
+  Expression *left;
+  Expression *right;
+};
+
+class Multiplication : public Expression
+{
+public:
+  Multiplication(Expression *_left, Expression *_right) : left{_left}, right{_right} {};
+  virtual double evaluate()
+  {
+    return left->evaluate() * right->evaluate();
+  };
+
+  virtual void setLeft(Expression *_left)
+  {
+    left = _left;
+  }
+  virtual void setRight(Expression *_right)
+  {
+    right = _right;
+  }
+
+private:
+  Expression *left;
+  Expression *right;
+};
+
+class Division : public Expression
+{
+public:
+  Division(Expression *_left, Expression *_right) : left{_left}, right{_right} {};
+  virtual double evaluate()
+  {
+    return left->evaluate() / right->evaluate();
+  };
+
+  virtual void setLeft(Expression *_left)
+  {
+    left = _left;
+  }
+  virtual void setRight(Expression *_right)
+  {
+    right = _right;
+  }
+
+private:
+  Expression *left;
+  Expression *right;
+};
+
+double Expression::evaluate()
+{
+  return -1;
+}
